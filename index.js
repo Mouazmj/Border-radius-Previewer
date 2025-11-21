f65e9be6bbf1a6e231cc5d87af87cef629dab854
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-21 23:06:26 +0100

    Now stting the rest of the inputs

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const topRight = document.getElementById('top-right')
 const topLeft = document.getElementById('top-left')
 const bottomRight = document.getElementById('bottom-right')
-const bottomLeft = document.getElementById('botton-left')
+const bottomLeft = document.getElementById('bottom-left')
 const preview = document.getElementById('preview')
 
 topRight.addEventListener('input', (inputValue) => {
     const value = inputValue.target.value
+    preview.style.borderTopRightRadius = `${value}px`
+})
+
+topLeft.addEventListener('input', (inputValue) => {
+    const value = inputValue.target.value
+    preview.style.borderTopLeftRadius = `${value}px`
+})
+
+bottomRight.addEventListener('input', (inputValue) => {
+    const value = inputValue.target.value
     preview.style.borderBottomRightRadius = `${value}px`
+})
+
+bottomLeft.addEventListener('input', (inputValue) => {
+    const value = inputValue.target.value
+    preview.style.borderBottomLeftRadius = `${value}px`
 })
