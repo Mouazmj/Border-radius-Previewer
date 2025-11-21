ff7f82a2b38818911d4e1f958e5f836bc8ba60d0
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-21 22:59:22 +0100

    Getting the id of the preview and listening with event

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const topRight = document.getElementById('top-right')
 const topLeft = document.getElementById('top-left')
 const bottomRight = document.getElementById('bottom-right')
 const bottomLeft = document.getElementById('botton-left')
+const preview = document.getElementById('preview')
+
+topRight.addEventListener('input', (inputValue) => {
+    const value = inputValue.target.value
+})
