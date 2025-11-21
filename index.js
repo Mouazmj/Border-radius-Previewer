9ee1f49520c9496cee5c70119619a4743b4c9e8d
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-21 22:38:00 +0100

    Getting all the corners input ids with dom

---
+++ index.js
@@ -0,0 +1,4 @@
+const topRight = document.getElementById('top-right')
+const topLeft = document.getElementById('top-left')
+const bottomRight = document.getElementById('bottom-right')
+const bottomLeft = document.getElementById('botton-left')
